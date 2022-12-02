12153935963dce5b805088e2367abc862e829a96
Arshim28 <98533618+Arshim28@users.noreply.github.com>
2022-12-02 09:55:59 +0530

    Update server.py

--- flower/server.py
+++ flower/server.py
@@ -1,40 +1,65 @@
+import numpy as np
+import pandas as pd
 import flwr as fl
-import utils
-from sklearn.metrics import log_loss
-from sklearn.linear_model import LogisticRegression
-from typing import Dict
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
+import warnings
+warnings.filterwarnings("ignore")
 
-def fit_round(server_round: int) -> Dict:
-    """Send round number to client."""
+def set_initial_parameters(model, num_features):
+    num_outputs=1
+    model.coef_=np.zeroes((num_features, ))
+
+def fit_round(server_round: int):
     return {"server_round": server_round}
 
+def set_model_params(model, params):
+    """Sets the parameters of a sklean LogisticRegression model."""
+    model.coef_ = params
+    return model
 
-def get_evaluate_fn(model: LogisticRegression):
+def get_evaluate_fn(model):
+
+    driver_df=pd.read_csv("driver.csv")
+    x_cols=['AGE', 'NUMBER_OF_TRIPS', 'REWARD_POINTS', 'MILES_IN_URBAN', 'CORNERING', 'SPEEDING', 'SEATBELT', 'DISTRACTION']
+    y_col="SAFETY_SCORE"
+
+    scaler=MinMaxScaler()
+    driver_df[x_cols]=scaler.fit_transform(driver_df[x_cols])
+
+    X_df=driver_df[x_cols]
+    Y_df=driver_df[y_col]/100
+
+    X_train, X_test, Y_train, Y_test=train_test_split(X_df, Y_df, test_size=0.3)
+
+    driver_linear_model=LinearRegression()
+    driver_linear_model.fit(X_df, Y_df)
+
     """Return an evaluation function for server-side evaluation."""
 
-    # Load test data here to avoid the overhead of doing it in `evaluate` itself
-    _, (X_test, y_test) = utils.load_mnist()
+    
 
     # The `evaluate` function will be called after every round
-    def evaluate(server_round, parameters: fl.common.NDArrays, config):
+    def evaluate(server_round, parameters, config):
+        
         # Update model with the latest parameters
-        utils.set_model_params(model, parameters)
-        loss = log_loss(y_test, model.predict_proba(X_test))
-        accuracy = model.score(X_test, y_test)
+        set_model_params(model, parameters)
+        loss = r2_score(Y_test, model.predict(X_test))
+        accuracy = model.score(X_test, Y_test)
         return loss, {"accuracy": accuracy}
 
     return evaluate
 
-
-# Start Flower server for five rounds of federated learning
-if __name__ == "__main__":
-    model = LogisticRegression()
-    utils.set_initial_params(model)
+if __name__=="__main__":
+    model=LinearRegression()
+    set_initial_parameters(model, 8)
     strategy = fl.server.strategy.FedAvg(
         min_available_clients=2,
         evaluate_fn=get_evaluate_fn(model),
-        on_fit_config_fn=fit_round,
+        on_fit_config_fn=fit_round
     )
     fl.server.start_server(
         server_address="0.0.0.0:8080",
